72669c9fa0df6413b056107fcb4cb0a827ad9053
wandzi <wandzel.mateusz@yahoo.com>
2020-10-26 11:32:21 +0100

    Initial commit.

--- src/components/layout.js
+++ src/components/layout.js
@@ -10,7 +10,8 @@ import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
-import "./layout.css"
+import "../styles/normalize.css"
+import "../styles/skeleton.css"
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
